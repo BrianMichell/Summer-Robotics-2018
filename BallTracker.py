38e84ccb52961bced2b1113b61daf955f473c3f9
BrianMichell <ecwhodie@yahoo.com>
2018-06-15 15:12:54 -0500

    Upgraded the tracker and started working with template matching

--- BallTracker.py
+++ BallTracker.py
@@ -1,8 +1,11 @@
 import cv2 as cv
 import numpy as np
 import ROI
+import time
 
 cap = cv.VideoCapture(0)
+ball = cv.imread('ball4.png')
+ball = cv.cvtColor(ball, cv.COLOR_BGR2GRAY)
 
 lowLight = True
 
@@ -19,12 +22,6 @@ ubLL = np.array([55, 160, 255])  # Upper bound
 lbHL = np.array([25, 125, 100])
 ubHL = np.array([35, 255, 200])
 
-# Old HSV bounding
-# lbLL = np.array([10,75,90]) #Lower bound
-# ubLL = np.array([45,255,255]) #Upper bound
-# lbHL = np.array([15,105,90])
-# ubHL = np.array([35,255,200])
-
 # No HSV bounding
 # lbHL = np.array([0,0,0])
 # ubHL = np.array([255,255,255])
@@ -33,14 +30,17 @@ kernel = np.ones((2, 2), np.uint8)
 
 tracker = []
 
+lastTime = time.time()
+
 while True:
     _, frame = cap.read()
+    # frame = cv.bilateralFilter(frame, 20, 25, 75)
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     
     if(lowLight):
         mask = cv.inRange(hsv, lbLL, ubLL)
         res = cv.bitwise_and(frame, frame, mask=mask)
-        # res = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
+        res = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
         # res = cv.inRange(res, 150, 255)
     else:
         mask = cv.inRange(hsv, lbHL, ubHL)
@@ -51,8 +51,11 @@ while True:
     closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
     # erode = cv.erode(res, kernel, iterations = 2)
     
+    # bilat = cv.bilateralFilter(res, 5, 10, 20)
+    
     # median = cv.medianBlur(closing, 9)
     median = cv.medianBlur(closing, 13)
+    # median = cv.medianBlur(erode, 13)
     # median = cv.medianBlur(closing, 21)
     
     edges = cv.Canny(median, 250, 255)
@@ -84,30 +87,68 @@ while True:
                     except (ZeroDivisionError):
                         pass
     
+    flag = False
+    
+    gs = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    itemsOfInterest = np.zeros(gs.shape, np.uint8)
+    # itemsOfInterest = np.zeros((480,640))
+    # itemsOfInterest[0:480, 0:640] = gs[0:480, 0:640]
     for elem in region:
+
         corner1, corner2 = elem.getPoints()
         cv.rectangle(frame, corner1, corner2, elem.color, 2)
         cv.putText(frame, 'Width: {}, Height: {}'.format(elem.w, elem.h), corner1, FONT, 1, elem.color, 1, AA)
+        
+        if(not flag):
+            y_min = corner1[1] - 5
+            y_min = max(0, y_min)
+            y_max = corner2[1] + 5
+            y_max = min(640, y_max)
+            x_min = corner1[0] - 5
+            x_min = max(0, x_min)
+            x_max = corner2[0] + 5
+            x_max = min(480, x_max)
+            
+            interest = gs[y_min:y_max, x_min:x_max]
+            itemsOfInterest[y_min:y_max, x_min:x_max] = interest
+            #cv.rectangle(itemsOfInterest, corner1, corner2, elem.color, 2)
+            
+            # flag = True
+            
         distance = PIX_PER_INCH / ((elem.w + elem.h) / 2)
         cv.putText(frame, 'Distance: {:.3} inches'.format(distance), corner2, FONT, 2, (0, 255, 0), 2, AA)
         cv.circle(frame, (elem.cx, elem.cy), 3, (255, 0, 0), -1)
         cv.putText(frame, '({},{})'.format(elem.cx, elem.cy), (elem.cx, elem.cy), FONT, 1, elem.color, 1, AA)
         tracker.append(elem.cx)
         tracker.append(elem.cy)
-        
-    '''
+    
+    matches = cv.matchTemplate(itemsOfInterest, ball, cv.TM_CCOEFF_NORMED)
+    balls = np.where(matches >= 0.7)
+    
+    for pt in zip(*balls[::-1]):
+        cv.circle(frame, (pt[0], pt[1]), 20, (255,255,255), -1)
+    
+    '''    
     length = len(tracker)
     for i in range(0, length, 2):
         cv.circle(frame, (tracker[i], tracker[i + 1]), 1, (0, 255, 0), -1)
-       
-    '''    
+    '''   
+    
+    # edges[0:100,0:100] = (255,255,255)
+    
+    cv.imshow('Items of interest', itemsOfInterest)
+        
     cv.imshow('frame', frame)
     cv.imshow('res', res)
     cv.imshow('mask', mask)
+    # cv.imshow('erode', erode)
     cv.imshow('closing', closing)
     cv.imshow('median', median)
     cv.imshow('edges', edges)
     cv.imshow('HSV masked', cv.bitwise_and(hsv, hsv, mask=mask))
+    
+    print('Frametime is {:.4} milliseconds'.format((time.time() - lastTime) * 1000))
+    lastTime = time.time()
     
     k = cv.waitKey(5) & 0xFF
     if(k == 27):
